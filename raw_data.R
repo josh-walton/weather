465358941194440f6cebe5bf1c12f5bf291c08b5
Josh Walton <joshwalton17@gmail.com>
2020-05-04 21:28:24 -0500

    Commit - Finally found the solution to show full months and facet wrap with year

--- raw_data.R
+++ raw_data.R
@@ -96,36 +96,24 @@ perfect.days <- day.summary %>%
 
 # Bar graph of perfect days per month
 
-# Previous code with year ####
-perf.days.count <- perfect.days %>% 
-  group_by(month = floor_date(date, "month")) %>% 
+perf.days <- perfect.days %>% 
+  group_by(Month = floor_date(date, "month")) %>% 
   count(date, sort = T) %>% 
   summarise(days = sum(n))
 
+perf.days <- perf.days %>% 
+  mutate(month.short = month(Month, label = T), year = year(Month))
 
-perf.days.count %>% 
-  ggplot(aes(x = month, y = days)) +
-  stat_summary(fun = sum, geom = "bar") +
-  facet_wrap(~year(month), scales = "free_x") +
-  scale_x_date(date_labels = "%b", date_breaks = "1 month")
-
-
-# With full months ####
-
-perf.days.count <- perfect.days %>% 
-  count(date, sort = T) %>% 
-  summarise(count = sum(n))
-
-perf.days.count <- perf.days.count %>% 
-  mutate(month = month(date, label = T)) %>% 
-  group_by(month) %>%
-  summarise(perf.days = sum(count)) %>% 
-  mutate(month = factor(month, levels = month.abb))
-  
-perf.days.count %>% 
-  ggplot(aes(month, perf.days)) +
+plot.perf.days <- perf.days %>% 
+  ggplot(aes(month.short, days)) +
   geom_bar(stat = "identity") +
-  scale_x_discrete(limits = month.abb)
+  scale_x_discrete(limits = month.abb) +
+  facet_wrap(~year)
+
+plot.perf.days
+
+
+
 
 
 
